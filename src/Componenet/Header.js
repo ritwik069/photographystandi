6f224ae1902cb7de638ad060bb124aadfbb5cbb3
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-05-08 13:34:17 +0530

    test

--- src/Componenet/Header.js
+++ src/Componenet/Header.js
@@ -1,8 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div className='h-[20vh] bg-black'>Header</div>
+    <div className='py-3  bg-black flex justify-center items-center'>
+      <div className='flex flex-col items-center justify-center'>
+        <div className='relative h-[50px] w-[50px]'>
+          <Image src={'/cameraicon.png'} fill className='bg-transparent object-contain' />
+
+        </div>
+        <h1 className='text-white text-[28px] fontfunky'>
+          Kartik Bhatia
+        </h1>
+        <h3 className='text-white text-[10px] tracking-[4px] font-semibold'>
+          PHOTOGRAPHY
+        </h3>
+      </div>
+    </div>
   )
 }
 
--- src/Componenet/MiddleComp.js
+++ src/Componenet/MiddleComp.js
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
-
+const link = 'https://photos.app.goo.gl/DdYuqgnoWhUAA8gp7'
 function MiddleComp({ Imagesrc }) {
   return (
     <div className='h-full flex flex-col items-center'>
-      <h1 className='font-[20px] mt-1'>Hi!</h1>
-      <h2 className='font-[20px] my-2'>Welcome to the wedding of </h2>
-      <h1 className='font-[24px] mb-2'>Sakshi and Naman</h1>
-      <div className='relative h-[150px] w-[80%] rounded-lg mb-3 '>
+      <h1 className='text-[20px] mt-1 fontibm'>Hi!</h1>
+      <h2 className='text-[20px] my-2 fontibm'>Welcome to the wedding of </h2>
+      <h1 className='text-[24px] mb-2 font-semibold fontsonara tracking-[3px]'>Sakshi and Naman</h1>
+      <div className='relative h-[180px] w-[90%] rounded-lg mb-3 '>
         <Image src={Imagesrc} fill className='objec-contain rounded-lg' />
         <div className='absolute w-full h-full flex justify-center items-center'>
-          <h1 className='text-white'>CLICK HERE</h1>
+          <h1 className='text-white fontbebeas text-[20px]' onClick={() => { window.open(link) }}>CLICK HERE</h1>
         </div>
       </div>
     </div>
